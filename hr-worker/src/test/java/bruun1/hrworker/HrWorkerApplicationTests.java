02968061465186a72f8a8bee310f4712833c1e5d
bruun21 <brunocrfraga@gmail.com>
2022-09-13 16:08:51 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/bruun1/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package bruun1.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
