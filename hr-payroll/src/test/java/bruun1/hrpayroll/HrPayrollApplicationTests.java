9bdbcd06c82adbbd4f3719f851def379a27b1f9a
bruun21 <brunocrfraga@gmail.com>
2022-09-19 14:31:36 -0300

    hr-payroll created

---
+++ hr-payroll/src/test/java/bruun1/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package bruun1.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
