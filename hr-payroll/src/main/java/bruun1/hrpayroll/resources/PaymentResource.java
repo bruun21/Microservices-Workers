bbf3c12c63ac50f97657ba24f79f5c9b02e1d4c2
bruun21 <brunocrfraga@gmail.com>
2022-09-20 14:31:01 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/bruun1/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/bruun1/hrpayroll/resources/PaymentResource.java
@@ -1,11 +1,15 @@
 package bruun1.hrpayroll.resources;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import bruun1.hrpayroll.entities.Payment;
 import bruun1.hrpayroll.services.PaymentService;
@@ -18,9 +22,16 @@ public class PaymentResource {
 	private PaymentService paymentService;
 	
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = paymentService.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		Payment payment = new Payment("Anakim", new BigDecimal(400.00) , days);
 		return ResponseEntity.ok(payment);
 	}
 }
--- hr-worker/src/main/java/bruun1/hrworker/services/WorkerService.java
+++ hr-worker/src/main/java/bruun1/hrworker/services/WorkerService.java
@@ -3,6 +3,7 @@ package bruun1.hrworker.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.aspectj.apache.bcel.util.ThreadLocalAwareRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,14 @@ public class WorkerService {
 	
 	
 	public Worker findById(Long id){
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		Optional<Worker> workerFound = repository.findById(id);
 		return workerFound.get();
 	}
