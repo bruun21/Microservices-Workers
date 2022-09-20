184c07be9b151ca0263b71be3f8dae91e2dc855a
bruun21 <brunocrfraga@gmail.com>
2022-09-20 11:40:33 -0300

    Configuring eureka server

---
+++ hr-eureka-server/src/test/java/bruun1/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bruun1.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
