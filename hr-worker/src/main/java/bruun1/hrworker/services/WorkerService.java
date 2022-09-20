2bc7b349fa1c356484bf5caebbb4c5a8d6a55eec
bruun21 <brunocrfraga@gmail.com>
2022-09-20 14:45:25 -0300

    Project hr-api-gateway-zuul created

--- hr-worker/src/main/java/bruun1/hrworker/services/WorkerService.java
+++ hr-worker/src/main/java/bruun1/hrworker/services/WorkerService.java
@@ -27,12 +27,14 @@ public class WorkerService {
 	
 	public Worker findById(Long id){
 
+		/*
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		*/
 		
 		Optional<Worker> workerFound = repository.findById(id);
 		return workerFound.get();
