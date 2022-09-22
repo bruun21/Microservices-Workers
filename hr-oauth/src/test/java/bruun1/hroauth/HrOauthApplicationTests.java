2d668c1577df3844706fccc1abc5742c4090fa28
bruun21 <brunocrfraga@gmail.com>
2022-09-22 16:57:03 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/bruun1/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package bruun1.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
