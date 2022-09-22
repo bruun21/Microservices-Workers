7b677d11660608796b2c3587ca03f4be716a42ba
bruun21 <brunocrfraga@gmail.com>
2022-09-22 10:32:08 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/bruun1/hruser/HrUserApplication.java
@@ -0,0 +1,13 @@
+package bruun1.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
---
+++ hr-user/src/test/java/bruun1/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package bruun1.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
